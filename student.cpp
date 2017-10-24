26c99699bb9bc39f5d17fcdf6d407b52206d59d5
GerelTselmeg <32637813+GerelTselmeg@users.noreply.github.com>
2017-10-24 08:59:19 +0800

    Add files via upload

--- student.cpp
+++ student.cpp
@@ -3,12 +3,13 @@
 
 using namespace std;
 
-class student {
+class student
+{
     char* sisiId;//оюутны дугаар(private тэмдэгтийн цуваа хадгалах хаяган хувьсагч)
     double GPA;//голч (private)
 public:
     student();//анхдагч байгуулагч: нэр, дугаарт санах ой нөөцөлж хоосон утга онооно, курсэд 1 гэсэн утга онооно.
-    student(char *n , char *id, int year_);//параметертэй байгуулагч: 3 гишүүн өгөгдөлд параметерээр утга авч онооно
+    student(char *n, char *id, int year_); //параметертэй байгуулагч: 3 гишүүн өгөгдөлд параметерээр утга авч онооно
     char* name;//нэр (public тэмдэгтийн цуваа хадгалах хаяган хувьсагч),
     int year;//курс (public)
     //гишүүн өгөгдөл тус бүрийн утгыг буцаах, утга оноох get, set функцууд
@@ -39,43 +40,53 @@ student::student()//анхдагч байгуулагч: нэр, дугаарт 
     char* name="";
     char* id="";
 }
-student::student(char *n , char *id, int year_){//параметертэй байгуулагч: 3 гишүүн өгөгдөлд параметерээр утга авч онооно
+student::student(char *n, char *id, int year_)  //параметертэй байгуулагч: 3 гишүүн өгөгдөлд параметерээр утга авч онооно
+{
     name = new char(sizeof(n)+1);
     sisiId = new char(sizeof(id)+1);
     this -> sisiId = id;
     this -> name = n;
     this -> year = year_;
 }
-void student::set_name(char *n){
+void student::set_name(char *n)
+{
     if(name!=NULL)
         delete []name;
     name = new char(sizeof(n)+1);
     this -> name = n;
 }
-void student::set_sisiId(char *id){
+void student::set_sisiId(char *id)
+{
     sisiId = new char(sizeof(id)+1);
     this -> sisiId = id;
 }
-void student::set_year(int a){
+void student::set_year(int a)
+{
     this ->year = a;
 }
-void student::set_gpa(double a){
+void student::set_gpa(double a)
+{
     this ->GPA = a;
 }
-char* student::get_name(){
+char* student::get_name()
+{
     return name;
 }
-char* student::get_sisiId(){
+char* student::get_sisiId()
+{
     return sisiId;
 }
-int student::get_year(){
+int student::get_year()
+{
     return year;
 }
-double student::get_gpa(){
+double student::get_gpa()
+{
     return GPA;
 }
 
-void student::copy_(student &s){//хуулагч байгуулагч: өөр объектийг хуулж үүснэ. ингэхдээ хаяган хувьсагч дээр гардаг асуудлыг бодолцох хэрэгтэй
+void student::copy_(student &s) //хуулагч байгуулагч: өөр объектийг хуулж үүснэ. ингэхдээ хаяган хувьсагч дээр гардаг асуудлыг бодолцох хэрэгтэй
+{
     name = new char(sizeof(s.get_name()+1));
     this ->name = s.get_name();
     sisiId = new char(sizeof(s.get_sisiId()+1));
@@ -84,29 +95,35 @@ void student::copy_(student &s){//хуулагч байгуулагч: өөр о
     this ->GPA = s.get_gpa();
 }
 
-bool student::greatThan(student &s){
+bool student::greatThan(student &s)
+{
     if(year>s.get_year())
         return true;
     else
         return false;
 }
-bool student::lessThan(student &s){
+bool student::lessThan(student &s)
+{
     return !greatThan(s);
 }
-bool student::isEqual(student &s){
+bool student::isEqual(student &s)
+{
     return year==s.get_year();
 }
-bool student::after(student &s){
+bool student::after(student &s)
+{
     if(strcmp(name,s.get_name())>0)
         return true;
     else
         return false;
 }
-bool student::before(student &s){
+bool student::before(student &s)
+{
     return !after(s);
 }
 
-student::~student(){
+student::~student()
+{
     delete []name;
     delete []sisiId;
 
@@ -115,8 +132,10 @@ void sort_student_name(student[], int);
 void sort_student_year(student[], int);
 void printall(student[], int);
 
-int main(){
-    student s[4] = {//олон оюутан үүсгэж объектон хүснэгтэд хадгал.
+int main()
+{
+    student s[4] =  //олон оюутан үүсгэж объектон хүснэгтэд хадгал.
+    {
         student("A","15b1seas1931",1),
         student("B","14b1seas1931",3),
         student("Z","15b1seas1932",2),
@@ -133,15 +152,21 @@ int main(){
 }
 void printall(student s[], int n)
 {
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++)
+    {
         cout << i << ":\t" << s[i].get_name() << "\t" << s[i].get_sisiId() << "\t" << s[i].get_year() << "\t" << s[i].get_gpa() << endl;
 
-    }cout << endl << endl << endl;
+    }
+    cout << endl << endl << endl;
 };
-void sort_student_name(student s[], int n){
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            if(s[i].after(s[j])){
+void sort_student_name(student s[], int n)
+{
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if(s[i].after(s[j]))
+            {
                 student temp;
                 temp.copy_(s[i]);
                 s[i].copy_(s[j]);
@@ -151,10 +176,14 @@ void sort_student_name(student s[], int n){
     }
 }
 
-void sort_student_year(student s[], int n){
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            if(s[i].greatThan(s[j])){
+void sort_student_year(student s[], int n)
+{
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if(s[i].greatThan(s[j]))
+            {
                 student temp;
                 temp.copy_(s[i]);
                 s[i].copy_(s[j]);
